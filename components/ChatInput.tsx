4da54662f755f2210fe4f45a9af1e7d771beb1c9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:53:53 +0530

    ChatInput Component The ChatInput component is responsible for rendering the input field where users can type and send messages in a chat interface. It includes options to send text messages, attach images, and use emojis.

---
+++ components/ChatInput.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React, { useState } from 'react'
+import { MdPhotoCamera } from "react-icons/md";
+import { EmojiPopover } from './EmojiPopover';
+import { useParams } from 'next/navigation';
+import { sendSnapMessage } from '@/lib/serveractions';
+import { Button } from './ui/button';
+import { Loader2 } from 'lucide-react';
+
+const ChatInput = () => {
+    
+
+    
+    
+}
+
