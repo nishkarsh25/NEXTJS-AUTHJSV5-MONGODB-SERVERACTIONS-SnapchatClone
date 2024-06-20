af462a322e3f0c9a6688aae235fd9ccae7ea483e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 08:33:21 +0530

    Checking Connection State The function first checks the isConnected variable. If it evaluates to true, it means a connection to MongoDB has already been established. In this case, it logs a message indicating that MongoDB is already connected and returns the isConnected variable which holds the Connection object.

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,9 @@ import mongoose , {Connection} from "mongoose";
 let isConnected:Connection | boolean = false;
 
 const connectDB = async () => {
-    
+    if(isConnected){
+        console.log("Mongo Already Connected");
+        return isConnected;
+    }
     
 }
