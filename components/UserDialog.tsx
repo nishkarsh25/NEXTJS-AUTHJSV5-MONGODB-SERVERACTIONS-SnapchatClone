e95fbff926dd20d0cbf6a89b2625deb70cc29c18
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:40:20 +0530

    Purpose The UserDialog component facilitates the selection of a user to whom a snap message (an image in this case) will be sent. It displays a dialog with a search input to filter users, a list of users to choose from, and buttons to send or cancel the message.

---
+++ components/UserDialog.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+
+import { UserDocument } from '@/models/user.model'
+import { Avatar, AvatarImage } from './ui/avatar'
+import { VscSend } from "react-icons/vsc";
+import { Loader2 } from 'lucide-react'
+import { sendSnapMessage } from '@/lib/serveractions'
+import { useRouter } from 'next/navigation'
+ 
+
+const UserDialog = (
+  ) => {
+
+  
+  
+
+  
+  
+
+  
+  
+}
+
