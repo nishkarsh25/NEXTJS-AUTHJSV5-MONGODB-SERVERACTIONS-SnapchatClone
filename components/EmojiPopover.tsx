9baed986550b536f9cd2fa2d68eba420f9973c6a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:55:11 +0530

    EmojiPopover Component The EmojiPopover component provides a popover UI for selecting emojis and sending them in a chat interface. It integrates with Next.js and utilizes various libraries and components for handling emoji selection and sending functionality.

---
+++ components/EmojiPopover.tsx
@@ -0,0 +1,20 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { MdEmojiEmotions } from "react-icons/md";
+import { EmojiArray } from "@/lib/emoji";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { useParams } from "next/navigation";
+import { readFileAsDataURL } from "@/lib/utils";
+import { sendSnapMessage } from "@/lib/serveractions";
+import { Loader2 } from "lucide-react";
+
+export function EmojiPopover() {
+  
+  
+  
+}
