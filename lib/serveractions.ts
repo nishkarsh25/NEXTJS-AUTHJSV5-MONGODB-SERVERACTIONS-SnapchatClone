28a8d14253ee7db68839891ed0bc038944df5f05
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 07:27:15 +0530

    Made the paths /login, /signup and added it into auth.ts file in pages array else it will show up default page for signIn i.e /signIn:
    Implemented the login, signup, logout functionality in the components itself
    
    and for logout functionality also used a server action to submit a form:

---
+++ lib/serveractions.ts
@@ -0,0 +1,27 @@
+"use server"
+import { auth, signOut } from "@/auth";
+import { Chat } from "@/models/chat.model";
+import { Message, MessageDocument } from "@/models/message.model"
+import { unstable_noStore as noStore } from "next/cache";
+import {v2 as cloudinary} from "cloudinary";
+import { revalidatePath } from "next/cache";
+import connectDB from "./db";
+import { redirect } from "next/navigation";
+
+cloudinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+})
+
+
+export const logoutHandler = async () => { 
+    try {
+        await signOut(); 
+    } catch (error) {
+        console.log(error);
+        throw error;
+        
+    }
+    redirect("/login");
+}
