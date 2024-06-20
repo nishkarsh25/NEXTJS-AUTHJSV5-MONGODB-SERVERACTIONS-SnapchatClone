d5730902ea41d0d88342502ac277bff04ab3e303
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 08:41:18 +0530

    Interfaces Two interfaces are defined:
    MessageInterface:
    
    Defines the structure of the Message document.
    It has five properties:
    senderId: Either a Types.ObjectId or a PopulatedDoc<UserDocument>. This indicates that senderId can be either an ObjectId referencing a document in the User collection or an already populated UserDocument.
    receiverId: Similar to senderId, indicating the recipient of the message.
    content: A string representing the content of the message. It is required (required: true).
    messageType: A string literal union ('text' | 'image') specifying the type of message.
    opened: A boolean indicating whether the message has been opened.
    MessageDocument:
    
    Extends MessageInterface and Document.
    Adds two additional properties:
    createdAt: A Date type indicating when the document was created.
    updatedAt: A Date type indicating when the document was last updated.

--- models/message.model.ts
+++ models/message.model.ts
@@ -1,5 +1,15 @@
 import mongoose, { Types, Document, Model, PopulatedDoc } from "mongoose"
 import { UserDocument } from "./user.model"
 
-
+export interface MessageInterface{
+    senderId:Types.ObjectId | PopulatedDoc<UserDocument>,
+    receiverId:Types.ObjectId | PopulatedDoc<UserDocument>,
+    content:string,
+    messageType: 'text' | 'image',
+    opened:boolean
+}
+export interface MessageDocument extends MessageInterface, Document{
+    createdAt:Date,
+    updatedAt:Date
+}
 
