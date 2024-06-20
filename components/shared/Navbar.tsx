3f95910e891bbc0a93ea194fa2d7b4874065f00a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 08:48:31 +0530

    Navbar Component Function Definition: Navbar is a functional component that asynchronously fetches authUser using await auth(). This means it waits for the authentication process to complete before rendering.
    Return Statement: The component returns JSX, which is the UI structure of the navbar:
    
    It consists of three main sections wrapped in a <div> container with the class flex items-center justify-between w-screen px-10 py-4, which uses Tailwind CSS utility classes for styling.

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -8,5 +8,8 @@ import Link from 'next/link';
 import LogoutButton from './LogoutButton';
 import { auth } from '@/auth';
 
-
+const Navbar = async () => {
+    const authUser = await auth();
+    
+}
 
