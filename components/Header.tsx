0c7c79ab96c21b9c53bfe4ca7d74d1b8ef2605d5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 08:51:35 +0530

    Imports The code imports necessary modules and components:
    React from 'react': The core library for React.
    Button from './ui/button': A custom UI component for buttons.
    MyAiPhoto from '@/public/myai-asset.png': This imports an image asset (myai-asset.png) from the public directory.
    Image from 'next/image': This is a Next.js component for optimizing images.
    MdOutlineLaptop from 'react-icons/md': This imports the MdOutlineLaptop icon from the react-icons/md package.
    auth from '@/auth': This is likely a function or module responsible for user authentication.
    PiChatCircleDots from 'react-icons/pi': This imports the PiChatCircleDots icon from the react-icons/pi package.
    Link from 'next/link': This is used for client-side navigation in Next.js applications.

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button';
+import MyAiPhoto from "@/public/myai-asset.png"
+import Image from 'next/image';
+import { MdOutlineLaptop } from "react-icons/md";
+import { auth } from '@/auth';
+import { PiChatCircleDots } from "react-icons/pi";
+import Link from 'next/link';
+
+
