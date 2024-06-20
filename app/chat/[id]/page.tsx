4fdf286370b8a78ea2128ffba0f42d4519fd1229
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:08:50 +0530

    Purpose Component Purpose: ChattingPage serves as a React functional component designed to facilitate the display and interaction of a chat interface between users.

---
+++ app/chat/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { auth } from '@/auth';
+import ChatPage from '@/components/ChatPage'
+import { getMessages, getUserProfile } from '@/lib/userdata'
+import React from 'react'
+
+const ChattingPage = async ({params}:{params:{id:string}}) => {
+  
+ 
+  
+}
+
