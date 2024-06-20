2459f1f16f9379023cbfc602d189a993e73d7b1a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:44:52 +0530

    ChatHeader Component The ChatHeader component is responsible for displaying the header section of a chat interface. It includes functionalities such as displaying the back button, user avatar and name, and a button to clear chat messages.

---
+++ components/ChatHeader.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react'
+import { IoIosArrowBack } from "react-icons/io";
+import { Avatar, AvatarImage } from './ui/avatar';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { deleteChatMessages } from '@/lib/serveractions';
+import { useParams } from 'next/navigation';
+import { useFormState, useFormStatus } from 'react-dom';
+import { Loader2 } from 'lucide-react';
+
+
+const ChatHeader = () => {
+    
+
+    
+ 
+    
+}
+
+
+
