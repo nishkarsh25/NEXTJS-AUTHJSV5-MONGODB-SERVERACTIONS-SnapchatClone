fadd0cd87ead38fb2e4d7781d870b6156f354452
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:27:55 +0530

    Purpose Component Purpose: The ChatSidebar component serves as a sidebar UI element in a chat application. It displays user information, provides a search bar for finding friends, and asynchronously loads and displays a list of friends.

---
+++ components/ChatSidebar.tsx
@@ -0,0 +1,10 @@
+import React, { Suspense } from 'react'; 
+import LogoutButton from './shared/LogoutButton';
+import SearchBar from './SearchBar';
+import Friends from './Friends';
+import FriendSkeleton from './FriendSkeleton';
+import { auth } from '@/auth';
+import { Avatar, AvatarImage } from './ui/avatar';
+
+
+
