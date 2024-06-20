b65b21807878f36a5a3e075034325459a6b06e2c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:38:58 +0530

    Purpose The ImagePreviewDialog component is responsible for displaying a dialog box that shows a preview of a selected image. It provides options to change the image, cancel the operation, or proceed to the next step.

---
+++ components/ImagePreviewDialog.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+} from "@/components/ui/dialog"
+import Image from 'next/image'
+
+const ImagePreviewDialog = (
+  ) => {
+ 
+  
+}
+
