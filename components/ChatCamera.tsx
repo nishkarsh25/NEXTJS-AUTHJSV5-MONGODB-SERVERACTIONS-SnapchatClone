1f35dc3d7c9841b78050d368fabbf42a1b9ddc67
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:37:01 +0530

    Purpose The ChatCamera component is responsible for rendering a camera-like interface within a chat interface, allowing users to select and preview images for sending.

---
+++ components/ChatCamera.tsx
@@ -0,0 +1,9 @@
+"use client"
+import React, { useEffect, useRef, useState } from 'react'
+import { CiCamera } from "react-icons/ci";
+import { readFileAsDataURL } from '@/lib/utils';
+import ImagePreviewDialog from './ImagePreviewDialog';
+import UserDialog from './UserDialog';
+
+
+
