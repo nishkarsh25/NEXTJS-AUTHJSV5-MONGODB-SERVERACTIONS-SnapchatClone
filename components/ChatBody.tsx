b172f7f5bc90ca8d8f4293a60f249fe4471498b1
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:51:11 +0530

    ChatBody Component The ChatBody component is responsible for rendering the main body of chat messages within the chat interface. It displays text messages and handles the preview of images sent as messages.

---
+++ components/ChatBody.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Image from 'next/image';
+import React, { useEffect, useRef, useState } from 'react'
+import { Dialog, DialogContent } from './ui/dialog';
+
+
+const ChatBody = () => {
+  
+
+  
+  
+}
+
