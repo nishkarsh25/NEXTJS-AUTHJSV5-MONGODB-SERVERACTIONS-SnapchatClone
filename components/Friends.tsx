fde66cc029d219adf6b843468ddb0b133a2625e0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:32:18 +0530

    Purpose Component Purpose: The Friends component fetches a list of users (excluding the authenticated user) from the backend and renders each user as a Friend component.

---
+++ components/Friends.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Friend from './Friend'
+import { auth } from '@/auth'
+import { getSidebarUsers } from '@/lib/userdata';
+ 
+// const sleep = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
+ 
+
+
