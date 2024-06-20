2e14361d40f394c0bfb653e90215acf2d7096660
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 09:33:53 +0530

    Purpose Component Purpose: The Friend component represents a UI element for displaying information about a user in a chat sidebar, including their profile picture, full name, and the status of their last message.

---
+++ components/Friend.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MdOutlineCameraAlt } from "react-icons/md";
+import { Avatar, AvatarImage } from './ui/avatar';
+import { formatDate } from '@/lib/utils';
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+import { RiCheckboxBlankFill } from "react-icons/ri";
+import { IoSend, IoSendOutline } from "react-icons/io5";
+import Link from 'next/link';
+
+
+
+
+
