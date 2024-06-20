871d4b9139582df6c2a9a78a6bd8f6e05e717042
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-20 08:48:12 +0530

    Imports The code imports necessary modules and components:
    Image from 'next/image': This is a Next.js component for optimizing images.
    React from 'react': The core library for React.
    SnapchatLogo from '@/public/Snapchat-logo.png': This imports the Snapchat logo image from the public directory.
    Input and Button from '../ui/input' and '../ui/button' respectively: These are custom UI components for input fields and buttons.
    TbGridDots from 'react-icons/tb': A specific icon component imported from the react-icons/tb package.
    Link from 'next/link': This is used for client-side navigation in Next.js applications.
    LogoutButton from './LogoutButton': A custom component that likely handles the logout functionality.
    auth from '@/auth': This is likely a function or module responsible for user authentication.

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+import SnapchatLogo from "@/public/Snapchat-logo.png";
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { TbGridDots } from "react-icons/tb";
+import Link from 'next/link';
+import LogoutButton from './LogoutButton';
+import { auth } from '@/auth';
+
+
+
